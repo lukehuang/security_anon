6995a2f963010324d72090512aba7c016d76b790
mstacey01 <mstacey01@gmail.com>
2018-07-26 11:53:03 -0600

    Add files via upload
    Initial commit

---
+++ src/test/java/com/wcf/securityform/SecurityformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wcf.securityform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
